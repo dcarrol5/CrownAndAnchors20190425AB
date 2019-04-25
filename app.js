f3ce3d4dd88595ecebbff3f98de942762b009d03
h22121 <h22121@lab.cpcc.edu>
2019-04-25 19:38:41 -0400

    CrownsAndAnchors20150425AB 738pm

---
+++ app.js
@@ -0,0 +1,21 @@
+var Face = require("./Face");
+var Bet = require("./Bet");
+var Mat = require("./Mat");
+
+try {
+    var myFace = new  Face('Crown', 10, 'http://images.clipartpanda.com/crown-clipart-yTkbAB4TE.svg');
+    var myBet = new Bet(myFace, -5);
+    var faces = [];
+
+    faces.push(myFace);
+
+    var myMat = new Mat(faces);
+
+    myMat.faces
+    console.log(`Im Beting ${myBet.betValue}`);
+}
+catch(err) {
+    console.log(err.message);
+    console.log(err.stack);
+}
+
